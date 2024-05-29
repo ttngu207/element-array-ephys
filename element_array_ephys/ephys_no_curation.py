ddb5351bd23a03f5a6a725d79c063b535b33c48b
Thinh Nguyen <thinh@datajoint.com>
2024-05-28 20:20:24 -0500

    chore: code cleanup

--- element_array_ephys/ephys_no_curation.py
+++ element_array_ephys/ephys_no_curation.py
@@ -824,16 +824,12 @@ class WaveformSet(dj.Imported):
 
         # Get electrode & channel info
         probe_info = (probe.Probe * EphysSessionProbe & key).fetch1()
-        electrode_query = probe.ElectrodeConfig.Electrode & probe_info
-
-        # Filter for used electrodes. If probe_info["used_electrodes"] is None, it means all electrodes were used.
-        number_of_electrodes = len(electrode_query)
-        probe_info["used_electrodes"] = (
-            probe_info["used_electrodes"]
-            if probe_info["used_electrodes"] is not None
-            else list(range(number_of_electrodes))
+        electrode_config_key = (probe.ElectrodeConfig & probe_info).fetch1("KEY")
+        electrode_query = (
+            probe.ProbeType.Electrode * probe.ElectrodeConfig.Electrode
+            & electrode_config_key
         )
-        electrode_query &= f"electrode IN {tuple(probe_info['used_electrodes'])}"
+
         channel2electrode_map: dict[int, dict] = {
             chn.pop("channel_idx"): chn for chn in electrode_query.fetch(as_dict=True)
         }  # e.g., {0: {'organoid_id': 'O09',
